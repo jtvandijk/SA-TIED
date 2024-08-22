c429dddf40641e44d35b07e3ab5ba8ee4de59524
jtvandijk <j.t.vandijk@ucl.ac.uk>
2024-08-22 12:02:33 +0100

    add solution s02

--- solutions/s01-assignment.R
+++ solutions/s01-assignment.R
@@ -5,21 +5,21 @@ library(tidyverse)
 library(haven)
 
 # read
-cpt <- read_csv('data/attributes/sa-language.csv')
-jhb <- read_dta('data/attributes/sa-internet-access.dta')
+cpt_att <- read_csv('data/attributes/sa-language.csv')
+jhb_att <- read_dta('data/attributes/sa-internet-access.dta')
   
 # prepare
-cpt <- cpt |>
+cpt_att <- cpt_att |>
   mutate(sp_xhosa_prop = sp_xhosa/sp_pop)
 
-jhb <- jhb |>
-  filter(dc_name == "City of Johannesburg") 
+jhb_att <- jhb_att |>
+  filter(dc_name == 'City of Johannesburg') 
 
-jhb <- jhb |>
+jhb_att <- jhb_att |>
   rowwise() |>
-  mutate(sal_pop = sum(across(starts_with("inter")), na.rm = TRUE))
+  mutate(sal_pop = sum(across(starts_with('inter')), na.rm = TRUE))
 
-jhb <- jhb |>
+jhb_att <- jhb_att |>
   group_by(sp_code) |>
   mutate(sp_pop = sum(sal_pop)) |>
   mutate(sp_inet = sum(inter_1)) |>
@@ -28,13 +28,13 @@ jhb <- jhb |>
   mutate(sp_inet_prop = sp_inet/sp_pop)
 
 # histogram - cpt
-hist(cpt$sp_xhosa, breaks = 50, xlab = "Number of sub places", main = "Number of isiXhosa speakers")
+hist(cpt_att$sp_xhosa, breaks = 50, xlab = 'Number of sub places', main = 'Number of isiXhosa speakers')
 
 # boxplot - cpt
-boxplot(cpt$sp_xhosa_prop, horizontal = TRUE)
+boxplot(cpt_att$sp_xhosa_prop, horizontal = TRUE)
 
 # histogram - jhb
-hist(jhb$sp_inet, breaks = 50, xlab = "Number of sub places", main = "Number of people with internet access at home")
+hist(jhb_att$sp_inet, breaks = 50, xlab = 'Number of sub places', main = 'Number of people with internet access at home')
 
 # boxplot - cpt
-boxplot(jhb$sp_inet_prop, horizontal = TRUE)
+boxplot(jhb_att$sp_inet_prop, horizontal = TRUE)
---
+++ solutions/s02-assignment.R
@@ -0,0 +1,49 @@
+# JTvD 2024
+
+# load
+library(tidyverse)
+library(haven)
+library(sf)
+library(tmap)
+
+# read
+jhb_att <- read_dta('data/attributes/sa-internet-access.dta')
+jhb_shp <- st_read('data/spatial/subplace-johannesburg-2013.gpkg')
+
+# prepare
+jhb_att <- jhb_att |>
+  filter(dc_name == 'City of Johannesburg') 
+
+jhb_att <- jhb_att |>
+  rowwise() |>
+  mutate(sal_pop = sum(across(starts_with('inter')), na.rm = TRUE))
+
+jhb_att <- jhb_att |>
+  group_by(sp_code) |>
+  mutate(sp_pop = sum(sal_pop)) |>
+  mutate(sp_inet = sum(inter_1)) |>
+  ungroup() |>
+  distinct(sp_code, sp_pop, sp_inet) |>
+  mutate(sp_inet_prop = sp_inet/sp_pop) |>
+  mutate(sp_code = as.character(sp_code))
+
+# join
+jhb_shp <- jhb_shp |>
+  left_join(jhb_att, by = c('sp_code' = 'sp_code'))
+
+# shape, polygons
+tm_shape(jhb_shp) +
+  
+  # specify column, colours
+  tm_polygons(
+    col= 'sp_inet_prop',
+    border.col = '#ffffff',
+    border.alpha = 0.3,
+    title = 'Internet at home',
+  ) +
+  
+  # set layout
+  tm_layout(
+    legend.outside = TRUE,
+    legend.position = c('left', 'top'),
+  ) 
